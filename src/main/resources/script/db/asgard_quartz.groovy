9ceb9cc5b784b7b63576ce456996e8dde17f93e2
flyleft <jcalaz@163.com>
2018-09-11 11:33:23 +0800

    [IMP] 修改liquibase中quartz.sql初始化路径为相对路径

--- src/main/resources/script/db/asgard_quartz.groovy
+++ src/main/resources/script/db/asgard_quartz.groovy
@@ -1,7 +1,7 @@
 package script.db
 
-//databaseChangeLog(logicalFilePath: 'asgard_quartz.groovy') {
-//    changeSet(id: '2018-09-06-create-tables-asgard_quartz', author: 'jcalaz@163.com') {
-//        sqlFile(path: '../quartz.sql', stripComments: true)
-//    }
-//}
+databaseChangeLog(logicalFilePath: 'asgard_quartz.groovy') {
+    changeSet(id: '2018-09-06-create-tables-asgard_quartz', author: 'jcalaz@163.com') {
+        sqlFile(path: './quartz.sql', relativeToChangelogFile: true, stripComments: true)
+    }
+}
