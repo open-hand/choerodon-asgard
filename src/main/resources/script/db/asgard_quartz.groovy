a9b92212c8c57ea1b0b0e08efc72f883717dbdb3
superlee <superleader8@gmail.com>
2018-09-12 17:41:04 +0800

    [FIX] 修改groovy脚本支持Oracle

--- src/main/resources/script/db/asgard_quartz.groovy
+++ src/main/resources/script/db/asgard_quartz.groovy
@@ -2,6 +2,10 @@ package script.db
 
 databaseChangeLog(logicalFilePath: 'asgard_quartz.groovy') {
     changeSet(id: '2018-09-06-create-tables-asgard_quartz', author: 'jcalaz@163.com') {
-        sqlFile(path: './quartz.sql', relativeToChangelogFile: true, stripComments: true)
+        if(helper.isOracle()) {
+            sqlFile(path: './quartz_oracle.sql', relativeToChangelogFile: true, stripComments: true)
+        } else {
+            sqlFile(path: './quartz.sql', relativeToChangelogFile: true, stripComments: true)
+        }
     }
 }
