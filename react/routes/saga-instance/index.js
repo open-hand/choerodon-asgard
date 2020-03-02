dcd72355d0e78bbb3827330ce89fb59d5ed863e6
isaac <273070111@qq.com>
2020-03-02 19:25:58 +0800

    [FIX]修改事物实例界面报错问题

--- react/routes/saga-instance/index.js
+++ react/routes/saga-instance/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { asyncRouter, nomatch } from '@choerodon/boot';
 
-const index = asyncRouter(() => import('./SagaInstance'));
+const SagaInstance = asyncRouter(() => import('./SagaInstance'));
 
 const Index = ({ match }) => (
   <Switch>
-    <Route exact path={match.url} component={index} />
+    <Route exact path={match.url} component={SagaInstance} />
     <Route path="*" component={nomatch} />
   </Switch>
 );
