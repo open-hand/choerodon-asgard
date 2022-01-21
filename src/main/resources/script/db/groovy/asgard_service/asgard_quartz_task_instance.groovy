4cfaad3d4b9b6e2a94b36f1b2513070535bc550f
18986 <changping.shi@hand-chian.com>
2022-01-21 15:08:11 +0800

    [ADD] 添加删除任务冗余数据

--- src/main/resources/script/db/groovy/asgard_service/asgard_quartz_task_instance.groovy
+++ src/main/resources/script/db/groovy/asgard_service/asgard_quartz_task_instance.groovy
@@ -76,4 +76,11 @@ databaseChangeLog(logicalFilePath: 'asgard_quartz_task_instance.groovy') {
             column(name: 'STATUS')
         }
     }
+
+    changeSet(id: '2022-01-21-delete-data', author: 'changping.shi@hand-china.com') {
+        sql("""
+            DELETE aqti FROM asgard_quartz_task_instance aqti WHERE aqti.task_id NOT IN ( SELECT id FROM asgard_quartz_task );
+            DELETE aqtm FROM asgard_quartz_task_member aqtm WHERE aqtm.task_id NOT IN ( SELECT id FROM asgard_quartz_task );
+        """)
+    }
 }
